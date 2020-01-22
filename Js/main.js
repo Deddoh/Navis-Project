317751bd6b77f97111d12bcb848ee446d5c3ebf1
Deddoh <deddoh84@gmail.com>
2020-01-22 21:31:39 +0300

    CFI script update

---
+++ Js/main.js
@@ -0,0 +1,88 @@
+
+// calling DOM elements
+// div that displays the selected table
+var table = document.getElementById('table_display');
+
+// table buttons
+var invoiceBtn = document.getElementById('invoice');
+var clientsBtn = document.getElementById('clients');
+var financeBtn = document.getElementById('finance');
+
+
+// click event listeners on the table buttons
+invoiceBtn.addEventListener('click', functionDisplayInvoice);
+function functionDisplayInvoice(){
+if(document.getElementById('invoiceTable').style.display === "none"){
+	document.getElementById('invoiceTable').style.display = "table";
+	document.getElementById('clientsTable').style.display = "none";
+	document.getElementById('financeTable').style.display = "none";
+}
+else{
+	document.getElementById('invoiceTable').style.display = "table";
+}
+};
+
+clientsBtn.addEventListener('click', functionDisplayClients);
+function functionDisplayClients(){
+if(document.getElementById('clientsTable').style.display === "none"){
+	document.getElementById('clientsTable').style.display = "table";
+	document.getElementById('invoiceTable').style.display = "none";
+	document.getElementById('financeTable').style.display = "none";
+}
+else{
+	document.getElementById('clientsTable').style.display = "none";
+}
+};
+
+
+financeBtn.addEventListener('click', functionDisplayFinance);
+function functionDisplayFinance(){
+if(document.getElementById('financeTable').style.display === "none"){
+	document.getElementById('financeTable').style.display = "table";
+	document.getElementById('clientsTable').style.display = "none";
+	document.getElementById('invoiceTable').style.display = "none";
+}
+else{
+	document.getElementById('financeTable').style.display = "none";
+}
+};
+
+
+// finance section
+
+
+// checkbox status
+
+// var checkbox = document.querySelector('confirm');
+
+// checkbox.addEventListener('change', functionDisplayButton);
+// 	function functionDisplayButton(){
+// 		if(checkbox.checked === true){
+// 			document.querySelectorAll('#confirmButtons').style.display = "table";
+
+// 		}
+// 		else{
+// 			document.querySelectorAll('#confirmButtons').style.display = "none";
+// 		}
+// 	}
+
+
+// var checkbox = document.querySelectorAll('.confirm');
+// var statusButtons = document.querySelectorAll('.confirmButtons');
+// for(var i = 0; i < checkbox.length; i++){
+// 	checkbox[i].addEventListener('change', functionDisplayButton);
+// 	function functionDisplayButton(){
+// 		if(checkbox.checked === true){
+// 			for(var j = 0; j < statusButtons.length; j++){
+// 			statusButtons.style.display = "table";
+
+// 		}}
+// 		// else{
+// 		// 	for(var j = 0; j < statusButtons.length; j++){
+// 		// 	statusButtons.style.display = "none";
+
+// 		// }
+			
+// 		//}
+// 		}
+// 	}
